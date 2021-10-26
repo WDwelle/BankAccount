81c37f1f19f4b80451ea7e181854191073ad21c4
Will Dwelle <willdwelle@frontier.com>
2021-10-26 13:30:41 -0700

    Users and accounts

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 class Bank_Account:
+    bank_name = "Dojo Bank"
     all_accounts = []
-    def __init__(self, bank_name, int_rate,):
-        self.name = bank_name
+    def __init__(self, balance, int_rate,):
         self.int_rate = int_rate
-        self.balance = 0
+        self.balance = balance
+        Bank_Account.all_accounts.append(self)
 
     def deposit(self, amount):
         self.balance += amount
@@ -25,15 +26,45 @@ class Bank_Account:
             print("Declined")
 
 
+Business = Bank_Account(500, .05)
+Saver = Bank_Account(200, .1)
+Investment = Bank_Account(1500, .35)
+
+#Investment.deposit(1000).deposit(500).deposit(3500).withdraw(1500).yeild_interest()
+
+#Business.deposit(1200).deposit(600).withdraw(20).withdraw(20).withdraw(78).withdraw(15).yeild_interest().display_account_info()
 
 
-Will = Bank_Account("Dojo Bank", .05)
-Gianna = Bank_Account("AnyBank", .1)
-Dad = Bank_Account("Money Planet", .35)
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = Bank_Account(0,.02)
+    # adding the deposit method
+    def make_deposit(self):
+        self.account.deposit(100)
+        print(self.account.balance)
+        return self
 
-Dad.deposit(1000).deposit(500).deposit(3500).withdraw(1500).yeild_interest()
+    def make_withdrawl(self):
+        self.account.withdraw(20)
+        print(self.account.balance)
+        return self
 
-Will.deposit(1200).deposit(600).withdraw(20).withdraw(20).withdraw(78).withdraw(15).yeild_interest().display_account_info()
+    def display_user_balance(self):
+        print(self.name , "$",self.account_balance)
+        return self
+
+    def transfer(self, amount, other_user):
+        self.account_balance -= amount
+        other_user.account_balance += amount
+        print(self.account_balance)
+        print(other_user.account_balance)
+        return self
+
+Will = User("Will Dwelle", "ww@coolguy.com")
+Gianna = User("Gianna Dwelle", "GW@selfcare.org")
+Dad = User("Denzil Dwelle", "dd@blah.com")
 
 
-
+Dad.make_deposit()
